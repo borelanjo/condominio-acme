96f65bbf9e0aa3b23d9c9f77bd33fea6c63ad9cb
Elton Leite <borelanjo@gmail.com>
2020-02-25 20:27:32 -0300

    Modifica script inicial

--- src/main/resources/db/migration/V01__ddl_initial.sql
+++ src/main/resources/db/migration/V01__ddl_initial.sql
@@ -4,7 +4,7 @@
 -- DROP TABLE public.tb_area_comum;
 
 CREATE TABLE public.tb_area_comum (
-  id int8 NOT NULL,
+  id bigserial NOT NULL,
   codigo int4 NULL,
   descricao varchar(255) NULL,
   CONSTRAINT tb_area_comum_pkey PRIMARY KEY (id)
@@ -15,7 +15,7 @@ CREATE TABLE public.tb_area_comum (
 -- DROP TABLE public.tb_condominio;
 
 CREATE TABLE public.tb_condominio (
-  id int8 NOT NULL,
+  id bigserial NOT NULL,
   email varchar(255) NULL,
   nome varchar(255) NULL,
   telefone varchar(255) NULL,
@@ -27,9 +27,9 @@ CREATE TABLE public.tb_condominio (
 -- DROP TABLE public.tb_avisos;
 
 CREATE TABLE public.tb_avisos (
-  id int8 NOT NULL,
+  id bigserial NOT NULL,
   descricao_aviso varchar(255) NULL,
-  id_condominio int8 NULL,
+  id_condominio bigint NULL,
   CONSTRAINT tb_avisos_pkey PRIMARY KEY (id)
 );
 
@@ -38,8 +38,8 @@ CREATE TABLE public.tb_avisos (
 -- DROP TABLE public.tb_condominio_avisos;
 
 CREATE TABLE public.tb_condominio_avisos (
-  condominio_id int8 NOT NULL,
-  avisos_id int8 NOT NULL,
+  condominio_id bigserial NOT NULL,
+  avisos_id bigserial NOT NULL,
   CONSTRAINT tb_condominio_avisos_pkey PRIMARY KEY (condominio_id, avisos_id),
   CONSTRAINT uk_bl9neb6ci6s2cpmc4ctbnd509 UNIQUE (avisos_id)
 );
@@ -49,11 +49,11 @@ CREATE TABLE public.tb_condominio_avisos (
 -- DROP TABLE public.tb_endereco;
 
 CREATE TABLE public.tb_endereco (
-  id int8 NOT NULL,
+  id bigserial NOT NULL,
   cep varchar(255) NULL,
   cidade varchar(255) NULL,
   rua varchar(255) NULL,
-  id_condominio int8 NULL,
+  id_condominio bigint NULL,
   CONSTRAINT tb_endereco_pkey PRIMARY KEY (id)
 );
 
@@ -62,14 +62,23 @@ CREATE TABLE public.tb_endereco (
 -- DROP TABLE public.tb_responsavel;
 
 CREATE TABLE public.tb_responsavel (
-  id int8 NOT NULL,
+  id bigserial NOT NULL,
   email varchar(255) NULL,
   nome varchar(255) NULL,
   telefone varchar(255) NULL,
+  CONSTRAINT tb_responsavel_pkey PRIMARY KEY (id)
+);
+
+-- Drop table
+
+-- DROP TABLE public.tb_unidade;
+
+CREATE TABLE public.tb_unidade (
+  id bigserial NOT NULL,
+  id_responsavel bigint NULL,
   bloco_unidade varchar(255) NULL,
   numero_unidade varchar(255) NULL,
-  id_responsavel int8 NULL,
-  CONSTRAINT tb_responsavel_pkey PRIMARY KEY (id)
+  CONSTRAINT tb_unidade_pkey PRIMARY KEY (id)
 );
 
 -- Drop table
@@ -77,12 +86,12 @@ CREATE TABLE public.tb_responsavel (
 -- DROP TABLE public.tb_multas;
 
 CREATE TABLE public.tb_multas (
-  id int8 NOT NULL,
+  id bigserial NOT NULL,
   data_multa date NULL,
   descricao_multa varchar(255) NULL,
   valor_multa numeric(19,2) NULL,
-  id_condominio int8 NULL,
-  id_unidade int8 NULL,
+  id_condominio bigint NULL,
+  id_unidade bigint NULL,
   CONSTRAINT tb_multas_pkey PRIMARY KEY (id)
 );
 
@@ -91,10 +100,10 @@ CREATE TABLE public.tb_multas (
 -- DROP TABLE public.tb_reclamacao;
 
 CREATE TABLE public.tb_reclamacao (
-  id int8 NOT NULL,
+  id bigserial NOT NULL,
   data_reclamacao date NULL,
   descricao_reclamacao varchar(255) NULL,
-  id_responsavel int8 NULL,
+  id_responsavel bigint NULL,
   CONSTRAINT tb_reclamacao_pkey PRIMARY KEY (id)
 );
 
@@ -103,12 +112,12 @@ CREATE TABLE public.tb_reclamacao (
 -- DROP TABLE public.tb_reserva;
 
 CREATE TABLE public.tb_reserva (
-  id int8 NOT NULL,
+  id bigserial NOT NULL,
   data_reserva date NULL,
   fim_reserva timestamp NULL,
   inicio_reserva timestamp NULL,
-  id_area_comum int8 NULL,
-  id_responsavel int8 NULL,
+  id_area_comum bigint NULL,
+  id_responsavel bigint NULL,
   CONSTRAINT tb_reserva_pkey PRIMARY KEY (id)
 );
 
@@ -116,8 +125,8 @@ ALTER TABLE public.tb_avisos ADD CONSTRAINT fk1fbc3o28g63m2khkpu3f4s1sy FOREIGN 
 ALTER TABLE public.tb_condominio_avisos ADD CONSTRAINT fkcc5uorqjak6y8jswqf5m4fhae FOREIGN KEY (condominio_id) REFERENCES tb_condominio(id);
 ALTER TABLE public.tb_condominio_avisos ADD CONSTRAINT fkoxxduxuotm6ak36anw85g6ih1 FOREIGN KEY (avisos_id) REFERENCES tb_avisos(id);
 ALTER TABLE public.tb_endereco ADD CONSTRAINT fkqqv8dvm5lcqm8bdlcyjyf5e76 FOREIGN KEY (id_condominio) REFERENCES tb_condominio(id);
-ALTER TABLE public.tb_responsavel ADD CONSTRAINT fkkpuu5n1vhg8ucpcvs6bsaiqnf FOREIGN KEY (id_responsavel) REFERENCES tb_responsavel(id);
-ALTER TABLE public.tb_multas ADD CONSTRAINT fk4sv502dohgrsts396fdcjmxav FOREIGN KEY (id_unidade) REFERENCES tb_responsavel(id);
+ALTER TABLE public.tb_unidade ADD CONSTRAINT fkkpuu5n1vhg8ucpcvs6bsaiqnf FOREIGN KEY (id_responsavel) REFERENCES tb_responsavel(id);
+ALTER TABLE public.tb_multas ADD CONSTRAINT fk4sv502dohgrsts396fdcjmxav FOREIGN KEY (id_unidade) REFERENCES tb_unidade(id);
 ALTER TABLE public.tb_multas ADD CONSTRAINT fkt4ksw5h1lki9eoo25taku31ad FOREIGN KEY (id_condominio) REFERENCES tb_condominio(id);
 ALTER TABLE public.tb_reclamacao ADD CONSTRAINT fkp0twyugeraeesqaaphpp1jwe2 FOREIGN KEY (id_responsavel) REFERENCES tb_responsavel(id);
 ALTER TABLE public.tb_reserva ADD CONSTRAINT fk590c0vnqss46ua8ws4mxq9pkk FOREIGN KEY (id_area_comum) REFERENCES tb_area_comum(id);
